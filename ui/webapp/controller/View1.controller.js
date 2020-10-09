f5c8c41c00f4bfdaef9da84e42a41dc637b96ee8
korayyersel <koray.yersel@q-perior.com>
2020-10-09 12:57:41 +0000

    start workflow from ui (first draft)

--- ui/webapp/controller/View1.controller.js
+++ ui/webapp/controller/View1.controller.js
@@ -1,7 +1,7 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageToast"
-], function (Controller,MessageToast) {
+], function (Controller, MessageToast) {
 	"use strict";
 
 	return Controller.extend("com.qperior.workflowsample.ui.controller.View1", {
@@ -10,13 +10,37 @@ sap.ui.define([
 		},
 		onPressStartWorkflow: function (evt) {
 			$.ajax({
-				url: "",
+				url: "/comqperiorworkflowsampleui/bpmworkflowruntime/v1/xsrf-token",
 				method: "GET",
 				headers: {
 					"X-CSRF-Token": "Fetch"
 				},
-				success:function(result, xhr,data){
-					
+				success: function (result, xhr, data) {
+					var token = data.getResponseHeader("X-CSRF-Token");
+					if (token === null) return;
+
+					$.ajax({
+						url: "/comqperiorworkflowsampleui/bpmworkflowruntime/v1/workflow-instances",
+						type: "POST",
+						data: JSON.stringify({
+							definitionId: "samplewf",
+							context: {
+								lastname: "yersel",
+								fistname: "koray"
+							}
+						}),
+						headers: {
+							"X-CSRF-Token": token,
+							"Content-Type": "application/json"
+						},
+						async: false,
+						success: function (data) {
+							MessageToast.show("The workflow has started");
+						},
+						error: function (data) {
+							MessageToast.show(data);
+						}
+					});
 				}
 			});
 		}
