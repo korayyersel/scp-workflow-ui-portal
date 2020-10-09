ff03f45a8e8d053b51fd6475e57e6e33168ed6ca
korayyersel <koray.yersel@q-perior.com>
2020-10-09 11:14:04 +0000

    workflow dependencies, view/controller draft added

--- ui/webapp/controller/View1.controller.js
+++ ui/webapp/controller/View1.controller.js
@@ -1,11 +1,24 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+], function (Controller,MessageToast) {
 	"use strict";
 
 	return Controller.extend("com.qperior.workflowsample.ui.controller.View1", {
 		onInit: function () {
 
+		},
+		onPressStartWorkflow: function (evt) {
+			$.ajax({
+				url: "",
+				method: "GET",
+				headers: {
+					"X-CSRF-Token": "Fetch"
+				},
+				success:function(result, xhr,data){
+					
+				}
+			});
 		}
 	});
 });
