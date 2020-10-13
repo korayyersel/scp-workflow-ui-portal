ad0fd93d542f95f774280de2ea877efb67f4dff3
korayyersel <koray.yersel@q-perior.com>
2020-10-13 12:41:23 +0000

    refactoring

--- ui/webapp/controller/TaskView.controller.js
+++ ui/webapp/controller/TaskView.controller.js
@@ -4,12 +4,12 @@ sap.ui.define([
 ], function (Controller,MessageToast) {
 	"use strict";
 
-	return Controller.extend("com.qperior.workflowsample.ui.controller.View2", {
+	return Controller.extend("com.qperior.workflowsample.ui.controller.TaskView", {
 
 		/**
 		 * Called when a controller is instantiated and its View controls (if available) are already created.
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-		 * @memberOf com.qperior.workflowsample.ui.view.View2
+		 * @memberOf com.qperior.workflowsample.ui.view.TaskView
 		 */
 		onInit: function () {
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
@@ -26,7 +26,7 @@ sap.ui.define([
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf com.qperior.workflowsample.ui.view.View2
+		 * @memberOf com.qperior.workflowsample.ui.view.TaskView
 		 */
 		//	onBeforeRendering: function() {
 		//
@@ -35,7 +35,7 @@ sap.ui.define([
 		/**
 		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf com.qperior.workflowsample.ui.view.View2
+		 * @memberOf com.qperior.workflowsample.ui.view.TaskView
 		 */
 		//	onAfterRendering: function() {
 		//
@@ -43,7 +43,7 @@ sap.ui.define([
 
 		/**
 		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf com.qperior.workflowsample.ui.view.View2
+		 * @memberOf com.qperior.workflowsample.ui.view.TaskView
 		 */
 		//	onExit: function() {
 		//
--- ui/webapp/controller/WorkflowStartView.controller.js
+++ ui/webapp/controller/WorkflowStartView.controller.js
@@ -4,7 +4,7 @@ sap.ui.define([
 ], function (Controller, MessageToast) {
 	"use strict";
 
-	return Controller.extend("com.qperior.workflowsample.ui.controller.View1", {
+	return Controller.extend("com.qperior.workflowsample.ui.controller.WorkflowStartView", {
 		onInit: function () {
 
 		},
