b077489712df23ef3fbdc27c8ab69976742d07af
korayyersel <koray.yersel@q-perior.com>
2020-10-16 12:44:13 +0000

    add second task and first draft for decisions

--- ui/webapp/Component.js
+++ ui/webapp/Component.js
@@ -1,51 +1,125 @@
 sap.ui.define([
-	"sap/ui/core/UIComponent",
-	"sap/ui/Device",
-	"com/qperior/workflowsample/ui/model/models"
+    "sap/ui/core/UIComponent",
+    "sap/ui/Device",
+    "com/qperior/workflowsample/ui/model/models"
 ], function (UIComponent, Device, models) {
-	"use strict";
+    "use strict";
 
-	return UIComponent.extend("com.qperior.workflowsample.ui.Component", {
+    return UIComponent.extend("com.qperior.workflowsample.ui.Component", {
 
-		metadata: {
-			manifest: "json"
-		},
+        metadata: {
+            manifest: "json"
+        },
 
 		/**
 		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
 		 * @public
 		 * @override
 		 */
-		init: function () {
-			// call the base component's init function
-			UIComponent.prototype.init.apply(this, arguments);
+        init: function () {
+            // call the base component's init function
+            UIComponent.prototype.init.apply(this, arguments);
 
-			// enable routing
-			this.getRouter().initialize();
-			
-			// look for task context and navigate to task ui automaticly
-			var startupParameters = this.getComponentData().startupParameters;
-			if(startupParameters && startupParameters.taskModel){
-				var taskModel = startupParameters.taskModel;
+            // enable routing
+            this.getRouter().initialize();
+
+            // look for task context and navigate to task ui automaticly
+            var startupParameters = this.getComponentData().startupParameters;
+            // check if the UI is loaded as a workflow task UI
+            if (startupParameters && startupParameters.taskModel) {
+                var taskModel = startupParameters.taskModel;
+                // get task header data
                 var taskData = taskModel.getData();
-                
-                if(taskData.Priority === "HIGH"){
-                    taskData.PriorityState  = "Warning";
+
+                if (taskData.Priority === "HIGH") {
+                    taskData.PriorityState = "Warning";
                 } else if (taskData.Priority === "VERY HIGH") {
                     taskData.PriorityState = "Error";
                 } else {
                     taskData.PriorityState = "Success";
                 }
                 taskData.CreatedOnStr = taskData.CreatedOn.toDateString();
+                // set data for task header model
                 this.getModel("taskHeaderData").setData(taskData);
+
+                var that = this;
+
+                var oNegativeAction = {
+                    sBtnTxt: "Reject",
+                    onBtnPressed: function (e) {
+                        that._completeTask(that.oComponentData.inboxHandle.attachmentHandle.detailModel.getData().InstanceID, "No")
+                    }
+                };
+
+                var oPositiveAction = {
+                    sBtnTxt: "Approve",
+                    onBtnPressed: function (e) {
+                        that._completeTask(that.oComponentData.inboxHandle.attachmentHandle.detailModel.getData().InstanceID, "Yes")
+                    }
+                };
+
+                // add task action buttons
+                startupParameters.inboxAPI.addAction({
+                    action: oNegativeAction.sBtnTxt,
+                    label: oNegativeAction.sBtnTxt,
+                    type: "Reject"
+                }, oNegativeAction.onBtnPressed);
+
+                startupParameters.inboxAPI.addAction({
+                    action: oPositiveAction.sBtnTxt,
+                    label: oPositiveAction.sBtnTxt,
+                    type: "Accept"
+                }, oPositiveAction.onBtnPressed);
+
+                var taskId = taskData.InstanceID;
+                var taskType = taskData.TaskDefinitionID;
+
+                // navigate to task view
+                if(taskData.TaskDefinitionName === "FirstTask"){
+                    this.getRouter().navTo("FirstTaskRoute", { taskId: taskId, taskType: taskType });
+                } else if(taskData.TaskDefinitionName === "SecondTask"){
+                    this.getRouter().navTo("SecondTaskRoute", { taskId: taskId, taskType: taskType });
+                }
                 
-				var taskId = taskData.InstanceID;
-				var taskType = taskData.TaskDefinitionID;
-				this.getRouter().navTo("TaskRoute", { taskId: taskId, taskType: taskType} );
-			}
-			
-			// set the device model
-			this.setModel(models.createDeviceModel(), "device");
-		}
-	});
+            }
+
+            // set the device model
+            this.setModel(models.createDeviceModel(), "device");
+        },
+
+        _completeTask: function (taskId, approvalStatus) {
+            var token = this._fetchToken();
+            $.ajax({
+                url: "/comqperiorworkflowsampleui/bpmworkflowruntime/v1/task-instances/" + taskId,
+                method: "PATCH",
+                contentType: "application/json",
+                async: false,
+                data: "{\"status\": \"COMPLETED\", \"context\": {\"FirstTaskResult\":\"" + approvalStatus + "\"}}",
+                headers: {
+                    "X-CSRF-Token": token
+                }
+            });
+            this._refreshTask(taskId);
+        },
+
+        _fetchToken: function () {
+            var token;
+            $.ajax({
+                url: "/comqperiorworkflowsampleui/bpmworkflowruntime/v1/xsrf-token",
+                method: "GET",
+                async: false,
+                headers: {
+                    "X-CSRF-Token": "Fetch"
+                },
+                success: function (result, xhr, data) {
+                    token = data.getResponseHeader("X-CSRF-Token");
+                }
+            });
+            return token;
+        },
+
+        _refreshTask: function (taskId) {
+            this.getComponentData().startupParameters.inboxAPI.updateTask("NA", taskId);
+        }
+    });
 });
--- workflow/workflows/samplewf.workflow
+++ workflow/workflows/samplewf.workflow
@@ -16,7 +16,10 @@
 			},
 			"activities": {
 				"2119779f-e4ef-463f-afb6-1c919bcaa098": {
-					"name": "UserTask1"
+					"name": "FirstTask"
+				},
+				"f1d8e2fd-663b-4b83-9798-0c8283688000": {
+					"name": "SecondTask"
 				}
 			},
 			"sequenceFlows": {
@@ -25,6 +28,9 @@
 				},
 				"9ea39617-54b2-4f15-8f6f-2ffd67d8435f": {
 					"name": "SequenceFlow2"
+				},
+				"eb662de7-66ef-4ea5-ab06-d0f55e550d03": {
+					"name": "SequenceFlow3"
 				}
 			},
 			"diagrams": {
@@ -43,13 +49,14 @@
 		},
 		"2119779f-e4ef-463f-afb6-1c919bcaa098": {
 			"classDefinition": "com.sap.bpm.wfs.UserTask",
-			"subject": "test1",
+			"subject": "First task in sample workflow",
 			"priority": "MEDIUM",
 			"isHiddenInLogForParticipant": false,
 			"userInterface": "sapui5://comqperiorworkflowsampleui/com.qperior.workflowsample.ui",
 			"recipientUsers": "koray.yersel@q-perior.com",
 			"id": "usertask1",
-			"name": "UserTask1"
+			"name": "FirstTask",
+			"documentation": "Documentation: First task in sample workflow"
 		},
 		"b6303f1d-5cb0-486f-95cd-a693103d03d8": {
 			"classDefinition": "com.sap.bpm.wfs.SequenceFlow",
@@ -63,7 +70,7 @@
 			"id": "sequenceflow2",
 			"name": "SequenceFlow2",
 			"sourceRef": "2119779f-e4ef-463f-afb6-1c919bcaa098",
-			"targetRef": "1753f448-f6f7-4ae9-b3cf-ff6ce1197ba9"
+			"targetRef": "f1d8e2fd-663b-4b83-9798-0c8283688000"
 		},
 		"4e4564a6-2db7-4207-9e7d-3d00ac5fd727": {
 			"classDefinition": "com.sap.bpm.wfs.ui.Diagram",
@@ -72,7 +79,9 @@
 				"cb6f2d03-83d0-4226-9051-b3c481feeb02": {},
 				"5c6b8a91-17de-407f-bf4b-62477dad1860": {},
 				"f953fbc9-8a50-4e75-9091-cf5117a9dd7a": {},
-				"f2ca562b-da73-4e78-b9e5-432809d3a059": {}
+				"f2ca562b-da73-4e78-b9e5-432809d3a059": {},
+				"5e82f995-971b-4a5b-b4d2-0aa4f726f2c1": {},
+				"c88d07c2-d4b7-41ed-93c0-4d653bc3fb2b": {}
 			}
 		},
 		"142b6a69-8708-4904-82c1-f4bf9244af4d": {
@@ -85,8 +94,8 @@
 		},
 		"cb6f2d03-83d0-4226-9051-b3c481feeb02": {
 			"classDefinition": "com.sap.bpm.wfs.ui.EndEventSymbol",
-			"x": 340,
-			"y": 100,
+			"x": 484,
+			"y": 125,
 			"width": 35,
 			"height": 35,
 			"object": "1753f448-f6f7-4ae9-b3cf-ff6ce1197ba9"
@@ -108,17 +117,50 @@
 		},
 		"f2ca562b-da73-4e78-b9e5-432809d3a059": {
 			"classDefinition": "com.sap.bpm.wfs.ui.SequenceFlowSymbol",
-			"points": "240,116.75 357.5,116.75",
+			"points": "240,116 304.25,116 304.25,130 368,130",
 			"sourceSymbol": "f953fbc9-8a50-4e75-9091-cf5117a9dd7a",
-			"targetSymbol": "cb6f2d03-83d0-4226-9051-b3c481feeb02",
+			"targetSymbol": "5e82f995-971b-4a5b-b4d2-0aa4f726f2c1",
 			"object": "9ea39617-54b2-4f15-8f6f-2ffd67d8435f"
 		},
 		"24053167-beb7-4cdc-b5b2-26e2a4f1bce5": {
 			"classDefinition": "com.sap.bpm.wfs.LastIDs",
-			"sequenceflow": 2,
+			"sequenceflow": 3,
 			"startevent": 1,
 			"endevent": 1,
-			"usertask": 1
+			"usertask": 2
+		},
+		"f1d8e2fd-663b-4b83-9798-0c8283688000": {
+			"classDefinition": "com.sap.bpm.wfs.UserTask",
+			"subject": "Second task in sample workflow",
+			"priority": "HIGH",
+			"isHiddenInLogForParticipant": false,
+			"userInterface": "sapui5://comqperiorworkflowsampleui/com.qperior.workflowsample.ui",
+			"recipientUsers": "koray.yersel@q-perior.com",
+			"id": "usertask2",
+			"name": "SecondTask",
+			"documentation": "Documentation: Second task in sample workflow"
+		},
+		"5e82f995-971b-4a5b-b4d2-0aa4f726f2c1": {
+			"classDefinition": "com.sap.bpm.wfs.ui.UserTaskSymbol",
+			"x": 318,
+			"y": 100,
+			"width": 100,
+			"height": 60,
+			"object": "f1d8e2fd-663b-4b83-9798-0c8283688000"
+		},
+		"eb662de7-66ef-4ea5-ab06-d0f55e550d03": {
+			"classDefinition": "com.sap.bpm.wfs.SequenceFlow",
+			"id": "sequenceflow3",
+			"name": "SequenceFlow3",
+			"sourceRef": "f1d8e2fd-663b-4b83-9798-0c8283688000",
+			"targetRef": "1753f448-f6f7-4ae9-b3cf-ff6ce1197ba9"
+		},
+		"c88d07c2-d4b7-41ed-93c0-4d653bc3fb2b": {
+			"classDefinition": "com.sap.bpm.wfs.ui.SequenceFlowSymbol",
+			"points": "368,130 451.25,130 451.25,142.5 501.5,142.5",
+			"sourceSymbol": "5e82f995-971b-4a5b-b4d2-0aa4f726f2c1",
+			"targetSymbol": "cb6f2d03-83d0-4226-9051-b3c481feeb02",
+			"object": "eb662de7-66ef-4ea5-ab06-d0f55e550d03"
 		}
 	}
 }
